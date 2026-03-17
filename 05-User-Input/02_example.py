bac34bb97c981e0627849cb75122de0e410e881d
ParthaHexIN <parth.kalita@zohomail.in>
2026-03-17 09:41:36 +0530

    User Input

---
+++ 05-User-Input/02_example.py
@@ -0,0 +1,10 @@
+# a = input ("Enter 1st Number:")
+# a = int(a) 
+# print(a + 10 )
+
+
+a = int (input("Enter 1st Number:")) # int() converts the text into an integer number.
+b = int (input("Enter 2nd number: "))
+print(a + b)
+
+# input() always returns a string.
