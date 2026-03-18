c7474a646bb0cf70897c0b4d2784d94cbf9a45df
ParthaHexIN <parth.kalita@zohomail.in>
2026-03-18 09:46:39 +0530

    Comment

---
+++ 06-Comment-esc-print/01_comment.py
@@ -0,0 +1,7 @@
+# print("my name is parth" )  #singel line comment
+
+
+# print ("Why Use Comments?" \                      #multi line comment 
+# "\nTo explain code logic" \                       #multi line comment 
+# "\nTo make code easier to understand" \           #multi line comment 
+# "\nTo improve readability and maintenance")       #multi line comment 
