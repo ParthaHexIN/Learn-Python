18a4a91ffaa50c0c8eb1e8e1f3ab7111a098029f
ParthaHexIN <parth.kalita@zohomail.in>
2026-03-20 21:10:51 +0530

    Arithmetic Operators

---
+++ 07-Day-Operators/01_ Arithmetic_Operators.py
@@ -0,0 +1,9 @@
+a = 5
+b = 2
+print("Addition:", a + b)
+print("Subtraction", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
