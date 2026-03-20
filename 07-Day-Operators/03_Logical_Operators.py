3186193d68d77f0d344391e2e3b4d72a8b6d543b
ParthaHexIN <parth.kalita@zohomail.in>
2026-03-20 21:32:28 +0530

    Logical Operator

---
+++ 07-Day-Operators/03_Logical_Operators.py
@@ -0,0 +1,17 @@
+# And logical operators
+
+a = 10
+b = 20
+print(a>5 and b>15)     #true
+print(a>15 and b>15)    #false
+
+
+# OR logical operator
+x = 5
+print(x > 10 or x < 10)   # true
+print(x > 10 or x < 3)    # false
+
+#Not logical Operator
+a = 10
+print(not(a > 5))
+print(not(a < 5))
