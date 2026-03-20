5ccac1823c9d8cc44a6232e101b449dc3b0bd718
ParthaHexIN <parth.kalita@zohomail.in>
2026-03-20 21:11:20 +0530

    Comparison Operators

---
+++ 07-Day-Operators/02_Comparison_Operators.py
@@ -0,0 +1,10 @@
+A =20
+B =30
+
+print (A == B)      #Equal To
+print (A != B)      #Not Equal To
+print (A > B)       #Greater Then
+print (A < B)       #Less Then
+print (A >= B)      #Greater Then Or Equal 
+print (A <= B)      #Less Then Or Equal 
+
