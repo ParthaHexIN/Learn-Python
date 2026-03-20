a6a360e25cfb28b0d0ac7e46ed16adbde5fa60ec
ParthaHexIN <parth.kalita@zohomail.in>
2026-03-20 08:44:16 +0530

    Solution

---
+++ Exercise 01 to 06/Solution.py
@@ -0,0 +1,95 @@
+# Exercise 1: Variables + Print
+
+# Name = "Partha"
+# Age = "20"
+# City = "Guwahati"
+# print("My name is ", Name)
+# print("I am", Age, "year old")
+# print("I live in", City)
+
+
+
+# Exercise 2: Data Types Check
+
+# A = 1
+# B = 1.0
+# C = "P"
+# D = True
+# print(type(A))      #integer
+# print(type(B))      #float
+# print(type(C))      #string
+# print(type(D))      #boolean
+
+
+
+# Exercise 3: Typecasting
+# a ="25"
+# b =10
+# a =(int(a)) #convert to integer
+# print(type(a))
+# print(type(b))
+# result= a + b
+# print(result)
+# print (type(result))
+
+
+
+# Exercise 4: User Input
+# a = (input("Enter Your Name:"))
+# b = (input("Enter Your Age:" ))
+# b = (int(b))    #convert to int 
+# print("Hello", a, ", Next Year You Will Be", b + 1)
+
+
+
+# Exercise 5: Comments + Escape + Print
+# print ( "Hello \"Partha\"  ")
+# print ("Welcome to Python")
+
+
+
+# # Exercise 6: Simple Calculator
+# a = float(input("Enter your 1st Number:-"))
+# b = float(input("Enter Your 2nd Number:-"))
+# print (a + b)
+# print (a / b)
+
+
+# # Exercise 7: Swap Variables
+# # a = 5
+# # b = 10
+
+# a = 5
+# b = 10
+# a,b = b,a
+# print ("a:-",a)
+# print ("b:-",b)
+
+
+
+# Exercise 8: Type Confusion Trap
+# x = "10"
+# y = "20"
+
+# x = "10"
+# y = "20"
+# print(x+y)
+# print (int(x) + int(y))
+
+
+
+# Exercise 9: Input + Type Logic + converting to float
+# a = input("Enter Your Number:-")
+# print(a)
+# print(type(a))
+# a = float(a)
+# print(a)
+# print(type(a))
+
+
+
+## Exercise 10: Multi-line Print (triple quotes OR `\n`)
+# Name= "Partha"
+# Age= "20"
+
+# print("Name:", Name, "\nAge:", Age, "\nCountry: India")
