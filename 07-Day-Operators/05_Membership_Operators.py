d152d93c0a4e402ea8589d930534c97557b0a652
ParthaHexIN <parth.kalita@zohomail.in>
2026-03-21 09:19:07 +0530

    Membership Operators

---
+++ 07-Day-Operators/05_Membership_Operators.py
@@ -0,0 +1,9 @@
+#IN example 
+Numbers= (1,2,3,4,5,6,)
+print(3 in Numbers)         #True 
+print(7 in Numbers)         #False
+
+#NOT IN Example
+Numbers= (1,2,3,4,5,6,)
+print(3 not in Numbers)     #false
+print(7 not in Numbers)     #True
