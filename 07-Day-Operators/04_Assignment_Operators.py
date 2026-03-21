e5445899938e0ef2912f34f00d0daab2772409e0
ParthaHexIN <parth.kalita@zohomail.in>
2026-03-21 09:18:39 +0530

    Assignment Operator

---
+++ 07-Day-Operators/04_Assignment_Operators.py
@@ -0,0 +1,36 @@
+x = 10
+print(x)
+
+x = 3
+x += 2
+print(x)
+
+x = 3
+x -= 2
+print(x)
+
+x = 3
+x *= 2
+print(x)
+
+x = 3
+x /= 2
+print(x)
+
+x = 3
+x // 2
+print(x)
+
+x = 3
+x %= 2
+print(x)
+
+x = 3
+x **= 2
+print(x)
+
+
+
+
+
+
