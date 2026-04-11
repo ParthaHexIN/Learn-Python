0f5a7588c42e56db984d99b9239b0d2e35a59fc8
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-04-11 20:20:57 +0530

    Create 04_Mini_Practice.py

---
+++ 13-Day-Break_Continue_Pass statements/04_Mini_Practice.py
@@ -0,0 +1,6 @@
+for x in range (1, 21):
+    if x == 13:
+        continue # continue skips the rest of the code in the loop for this iteration
+    if x == 17:
+        break # break exits the loop entirely
+    print (x)
