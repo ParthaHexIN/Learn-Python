8958f409f7f2329a7ac4a16977d292b1cb08797c
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-04-12 08:52:01 +0530

    01_Exercise

---
+++ Exercise 01 to 13/01_Exercise.py
@@ -0,0 +1,34 @@
+# If-Else Conditional Statements - Practice Questions
+# 1. Positive, Negative, or Zero 
+
+# i = int (input ("Enter a Number :"))
+# if i > 0:
+#     print ("The Number is Positive")
+# elif i < 0:
+#     print ("The Number is Negative")
+# else:
+#     print ("The number is Zero")
+
+
+
+
+
+# 2. Voting Eligibility
+
+# age = int(input("Enter Your Age: "))
+
+# if age >= 18:
+#     print("You are eligible for vote")
+# else:
+#     print("You are not eligible for vote")
+
+
+
+
+# 3. Even or Odd
+
+# num= int (input("Enter a number: "))
+# if num %2 == 0:
+#     print ("The Number Is Even ")
+# else:
+#     print ("The Number is Odd")
