c43189307fb34f8c7c15ce01e8237e991cab2e86
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-05-11 09:46:59 +0530

    Create Solution 2.py

---
+++ Exercise 01 to 13/Solution 2.py
@@ -0,0 +1,43 @@
+# 2. Match Case Statements - Practice Questions
+
+
+# 2.1 day of the week
+
+
+# day = int(input("Enter a day number (1-7): "))
+
+# match day:
+#     case 1:
+#         print("Monday")
+#     case 2:
+#         print("Tuesday")
+#     case 3:
+#         print("Wednesday")
+#     case 4:
+#         print("Thursday")
+#     case 5:
+#         print("Friday")
+#     case 6:
+#         print("Saturday")
+#     case 7:
+#         print("Sunday")
+#     case _:
+#         print("Invalid day")
+
+
+# 2.2 Simple Calculator using Match Case
+
+
+# num1 = int (input("Enter first number :"))
+# num2 = int (input("Enter Second number :"))
+
+# operation =input ("choose an operation (=,-,*,/):")
+# match  operation:
+#     case "+":
+#         print (num1 + num2)  
+#     case "-":
+#         print (num1 - num2)
+#     case "*":
+#         print (num1 * num2)
+#     case "/":
+#         print (num1 / num2)
