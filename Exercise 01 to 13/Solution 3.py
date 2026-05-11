ce81bd444f33f31ca77e62800a78e3b5ce67fc94
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-05-11 09:47:08 +0530

    Create Solution 3.py

---
+++ Exercise 01 to 13/Solution 3.py
@@ -0,0 +1,29 @@
+# ## 1. Print Numbers from 1 to 10 
+# for i in range(1,11):
+#     print(i)
+
+
+# ## 2. Multiplication Table
+# n = int(input ("Enter Your Number:"))
+# for i in range(1,11):
+#     print (n,"X",i,"=",n*i)
+
+
+# ## 3. Sum of Numbers from 1 to 100
+
+# total = 0
+# for i in range(1,101):
+#     total += i
+# print (total)
+
+
+## 4. Pattern Printing
+# ```
+# *
+# **
+# ***
+# ****
+# ```
+
+for i in range(1,5):
+    print("*"*i)
