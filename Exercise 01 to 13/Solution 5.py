6b927443d4b581afea104ab2729524bc9448e4d4
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-05-13 18:09:53 +0530

    Create Solution 5.py

---
+++ Exercise 01 to 13/Solution 5.py
@@ -0,0 +1,35 @@
+
+# ## 1. Using break
+
+# for i in range(1,11):
+#     if (i ==7):
+#         break
+#     print (i)
+
+
+# ## 2. Using continue
+
+# for i in range (1,11):
+#     if (i==5):
+#         continue
+#     print (i)
+
+
+# ## 3. Using pass
+
+# for i in range(1, 11):
+#     match i:
+#         case 1:
+#             print(1)
+
+#         case 2:
+#             print(2)
+
+#         case 3:
+#             pass
+
+#         case 4:
+#             print(4)
+
+#         case 5:
+#             print(5)
