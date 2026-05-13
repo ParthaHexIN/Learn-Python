80eb1a517ea8820c61e3ac08926516ac2432bd74
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-05-13 18:09:48 +0530

    Create Solution 4.py

---
+++ Exercise 01 to 13/Solution 4.py
@@ -0,0 +1,25 @@
+# ## 1. Print Numbers from 1 to 10
+# sum = 0 
+# i = 1 
+
+# while i <=10:
+#     print(i)
+#     sum +=i
+#     i+=1
+# print(sum)
+
+
+# ## 2. Password Checker
+# password = "partha"
+# Entered_Password = input ("Enter the Password:")
+
+# while Entered_Password != password:
+#     Entered_Password = input ("Try Again and Enter the Password:")
+
+# print("Access Granted!")
+
+
+
+# ## 3. Reverse a Number
+# num = 65222
+# print (int(str(num)[::-1]))
