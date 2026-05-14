76899757876d9b6d9658da81f8fc03454482ab02
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-05-14 09:36:14 +0530

    Create 02_indexing.py

---
+++ 14-Day-Strings/02_indexing.py
@@ -0,0 +1,19 @@
+Name = "Partha"
+
+# name = "P   a   r   t   h   a"
+#         0   1   2   3   4   5   
+#        -6  -5  -4  -3  -2  -1
+
+# print (Name [0])    # p
+# print (Name [1])    # a
+# print (Name [2])    # r
+# print (Name [3])    # t
+# print (Name [4])    # h
+# print (Name [5])    # a
+
+print (Name [-1])   # a     #name [-1+6] = name [5]
+print (Name [-2])   # h     #name [-2+6] = name [4]
+print (Name [-3])   # t     #name [-3+6] = name [3]
+print (Name [-4])   # r     #name [-4+6] = name [2]
+print (Name [-5])   # a     #name [-5+6] = name [1]
+print (Name [-6])   # p     #name [-6+6] = name [0]
