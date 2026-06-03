06b7761aefd404b5bd64602a1963589f12034e08
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-06-03 08:29:48 +0530

    Create 01_slicing.py

---
+++ 15-Day-String_Slicing_Indexing/01_slicing.py
@@ -0,0 +1,11 @@
+name = "Partha0123456789"
+print (name [0:2])
+print (name [1:8])
+
+print (name [2:12:1]) #skip 0 charaters 
+print (name [2:12:2]) #skip 1 charaters
+print (name [2:12:3]) #skip 2 charaters
+
+print (name [:4]) #replace 0 with blank
+print (name [4:]) #replace 12 with blank
+print (name [::2]) #skip 1 charaters
