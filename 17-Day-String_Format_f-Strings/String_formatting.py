cf7eeb7a65c76bb313aa911ca4dd9c37542ec5d0
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-06-09 11:08:30 +0530

    Create String_formatting.py

---
+++ 17-Day-String_Format_f-Strings/String_formatting.py
@@ -0,0 +1,13 @@
+
+template = "Dear {}, You are awesome. Take this {}$ bag"
+a = "John"
+a1 = 1000
+b = "Hirok"
+b1 = 100
+c = "Deep"
+c2 = 500
+
+s1 = template.format(a, a1)
+print(s1)
+
+print(f"{b} you are awesome and take this {b1}$ bag")
