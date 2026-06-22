4e5fc426418e005fc44e42ab8ee75a7bb2863f00
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-06-22 12:06:25 +0530

    Create 01_Lambda_Function.py

---
+++ 20-Day-Lambda Functions/01_Lambda_Function.py
@@ -0,0 +1,28 @@
+# add = lambda a,b:a+b            # Add Two Numbers
+# print (add(5,3))
+
+
+# square = lambda x:x*x          # Square of a Number
+# print (square(5))
+
+# is_even = lambda x: x % 2 ==0       # Check Even or Odd
+# print (is_even (4))
+# print (is_even (5))
+
+
+# maximum = lambda a,b: a if a>b else b        # Maximum of Two Numbers
+# print (maximum (20,25)) 
+
+# number = [1,2,3,4,5]                                    # Use Lambda with map()
+# result = list(map (lambda x: x * 2 , number))
+# print (result)
+
+
+# number2 = [1,2,3,4,5,6,7,8,9]                              # Use Lambda with filter()
+# Even_numbers = list(filter(lambda x: x %2 == 0, number2))
+# print(Even_numbers)
+
+
+# student = [("Rahul", 85),("priya",95),("aman",54)]          #Sort a List of Tuples Using Lambda
+# student.sort (key= lambda x:x[1])
+# print (student)
