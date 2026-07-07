04c78802b42211f3a6dd0d97c986d8e224ce60bd
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-07-07 16:39:49 +0530

    Update 01_List.py

--- 25-Day-List/01_List.py
+++ 25-Day-List/01_List.py
@@ -1,4 +1,27 @@
-name= ["Ram","Sam","Dham"]
-print (name)
+# name= ["Ram","Sam","Dham"]
+# print (name)
 
-print (name [1])
+# print (name [1])
+
+fruite =[ "Apple","Banana","Mango","Grapes"]
+print (fruite)
+print (fruite[2])
+
+
+data = [10, "Python", 3.14, True]
+print(data)
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(matrix)
+
+
+
+numbers = list(map(int, input("Enter numbers: ").split()))
+
+print(numbers)
