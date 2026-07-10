a1656727bf32c5c6926d9edb2bd5441e21cf17e9
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-07-10 07:00:30 +0530

    Update 02_List Methods.py

---
+++ 25-Day-List/02_List Methods.py
@@ -0,0 +1,30 @@
+Number= [1, 2, 3, 4, 5]
+print(Number)
+
+# 1.append
+Number.append(6)
+print(Number)
+
+# 2.insert
+Number.insert(1,99)
+print(Number)
+
+# 3.remove
+Number.remove(3)
+print(Number)
+
+# 4.pop
+Number.pop(2)
+print(Number)
+
+# 5.reverse
+Number.reverse()
+print(Number)
+
+# 6.sort
+Number.sort()
+print(Number)
+
+# 7.clear
+Number.clear()
+print(Number)
