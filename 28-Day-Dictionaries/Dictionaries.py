1e0cbf68e4401731e18d40d09660e8183dbbaa21
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-07-14 18:43:31 +0530

    Create Dictionaries.py

---
+++ 28-Day-Dictionaries/Dictionaries.py
@@ -0,0 +1,20 @@
+country = {
+    "name": "India",
+    "capital": "New Delhi",
+    "currency": "Indian Rupee",
+    "population": "1.4 Billion"
+}
+
+print(country)
+
+
+
+
+# product = {
+#     "product_id": 501,
+#     "name": "Keyboard",
+#     "price": 1200,
+#     "stock": 45
+# }
+
+# print(product)
