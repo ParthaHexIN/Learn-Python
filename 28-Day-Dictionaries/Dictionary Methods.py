27e7112e459ad734e62768c10d598df1a546cde6
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-07-15 20:41:27 +0530

    Create Dictionary Methods.py

---
+++ 28-Day-Dictionaries/Dictionary Methods.py
@@ -0,0 +1,34 @@
+student = {
+    "name": "Partha",
+    "age": 20,
+    "course": "Python"
+}
+
+print(student.keys())
+
+
+student = {
+    "name": "Partha",
+    "age": 20,
+    "course": "Python"
+}
+
+print(student.values())
+
+
+
+student = {
+    "name": "Partha",
+    "age": 20,
+    "course": "Python"
+}
+
+print(student.items())
+
+student = {
+    "name": "Partha",
+    "age": 20
+}
+
+print(student.get("name"))
+print(student.get("course"))
