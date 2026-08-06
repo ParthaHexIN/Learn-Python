c6d183831cf0b9f54cdf2d3c046908fb965e00bc
Partha Pratim Kalita <parth.kalita@zohomail.in>
2026-08-05 21:30:12 -0700

    Create 01_Write.py

---
+++ 31-Day-Read_Write_Append/01_Write.py
@@ -0,0 +1,18 @@
+# try:
+#     F = open("Read.txt", "r")
+#     content = F.read()
+#     print(content)
+#     F.close()
+
+# except FileNotFoundError:
+#     print("File not found.")
+    
+    
+f = open("parth.txt", "W")
+content = f.read()
+print(content)
+f.close()
+
+    
+    
+    
